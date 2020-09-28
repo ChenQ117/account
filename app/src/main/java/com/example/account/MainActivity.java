188012d33df86004d76b12b1bfcf786bf409b2a7
ChenQ117 <2294874121@qq.com>
2020-09-28 21:06:25 +0800

    3

--- app/src/main/java/com/example/account/MainActivity.java
+++ app/src/main/java/com/example/account/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Intent intent = new Intent(this,Main2Activity.class);
-        startActivity(intent);
+        //startActivity(intent);
 
     }
 }
