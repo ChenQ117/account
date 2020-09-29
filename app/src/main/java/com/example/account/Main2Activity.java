7a3fab13e5bfa410a81b5d848b34f808718e7c32
ChenQ117 <2294874121@qq.com>
2020-09-29 22:01:02 +0800

    3

--- app/src/main/java/com/example/account/Main2Activity.java
+++ app/src/main/java/com/example/account/Main2Activity.java
@@ -11,6 +11,6 @@ public class Main2Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_input_layout);
+        setContentView(R.layout.who_pay_layout);
     }
 }
