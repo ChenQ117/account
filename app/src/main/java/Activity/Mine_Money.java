a6dbd846d27183cf0dc0f51dede3117124685ac0
chaos <741386204@qq.com>
2020-09-30 19:21:59 +0800

    toolbar commit

--- app/src/main/java/Activity/Mine_Money.java
+++ app/src/main/java/Activity/Mine_Money.java
@@ -11,5 +11,6 @@ public class Mine_Money extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mine_layout);
+
     }
 }
--- app/src/main/java/com/example/account/MainActivity.java
+++ app/src/main/java/com/example/account/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.account;
 
+import Activity.Mine_Money;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import Activity.Item_Input;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,8 +17,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         Intent intent = new Intent(this, Item_Input.class);
         startActivity(intent);
 
+
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.tool, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.mine:
+                Intent intent = new Intent(MainActivity.this, Mine_Money.class);
+                startActivity(intent);
+                break;
+        }
+        return true;
     }
 }
