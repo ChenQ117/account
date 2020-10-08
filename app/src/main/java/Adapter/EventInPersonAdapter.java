fe3ff006ac625786a898861253aa9c9c17da6dc6
Infinity410 <444152364@qq.com>
2020-10-08 15:03:16 +0800

    debug

--- app/src/main/java/Adapter/EventInPersonAdapter.java
+++ app/src/main/java/Adapter/EventInPersonAdapter.java
@@ -55,7 +55,7 @@ public class EventInPersonAdapter extends RecyclerView.Adapter<EventInPersonAdap
     @Override
     public MyViewHoder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        View view = inflater.inflate(R.layout.name_layout, null, false);
+        View view = inflater.inflate(R.layout.name_layout, parent, false);
         return new MyViewHoder(view);
     }
 
